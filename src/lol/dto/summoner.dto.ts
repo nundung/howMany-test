dd7e77783bc535f20ed109a3d412d335a1c51294
nundung <jjy809123@naver.com>
2024-06-17 22:05:02 +0900

    Feat: userDto, summonerDto생성

---
+++ src/lol/dto/summoner.dto.ts
@@ -0,0 +1,17 @@
+export class SummonerDto {
+  puuid: string;
+
+  id: string;
+
+  accountId: string;
+
+  profileIconId: number;
+
+  nickname: string;
+
+  tag: string;
+
+  revisionDate: Date;
+
+  level: number;
+}
