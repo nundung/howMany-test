2017ec0e877ca9adac8536345a1df57506dac276
nundung <jjy809123@naver.com>
2024-06-17 22:06:22 +0900

    Feat: getUser 매서드 수정

--- src/lol/lol.controller.ts
+++ src/lol/lol.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { LolService } from './lol.service';
-import { GetUserDto } from './dto/getUser.dto';
+import { AccountDto } from './dto/account.dto';
 
 @Controller('lol')
 export class LolController {
   constructor(private lolService: LolService) {}
 
   @Get('user')
-  async getUser(@Query() getUserDto: GetUserDto) {
-    return await this.lolService.getUserPuuId(getUserDto);
+  async getUser(@Query() accountDto: AccountDto) {
+    const puuid = await this.lolService.getUserPuuid(accountDto);
+
+    return await this.lolService.getSummoner(puuid);
   }
 }
